e05a113573480b4360726e9bb8bdee2d62c1a456
Zimina Ivanna <ivanna.zimina@plus4u.net>
2023-01-30 18:36:53 +0200

    posr request was added

--- src/components/calendar-current-date/index.js
+++ src/components/calendar-current-date/index.js
@@ -1,6 +1,8 @@
 import { Calendar } from "react-date-range";
+import { httpCommon } from "../../server-settings/http-common";
 import moment from "moment";
 import { setCurrentMonth } from "../../store/calendar-creator";
+import { v4 as uuidv4 } from "uuid";
 import React, { useState, useEffect, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import style from "./style.module.scss";
@@ -14,6 +16,8 @@ export const CalendarCurrentDate = () => {
 
     const [calendar, setCalendar] = useState("");
     const [open, setOpen] = useState(false);
+    const [showPostMessage, setShowPostMessage] = useState(false);
+    const [postMessage, setPostMessage] = useState("");
     const refOne = useRef(null);
 
     const currentDate = moment().format("DD/MM/YYYY");
@@ -48,27 +52,63 @@ export const CalendarCurrentDate = () => {
         }
     };
 
+    const sendDateHandler = async () => {
+        try {
+            const id = uuidv4();
+            const data = await httpCommon.post("/dates", {
+                id: id,
+                date: moment.utc(calendar).format(),
+            });
+
+            if (data.status === 201) {
+                setPostMessage("Date was added successfully!");
+                setShowPostMessage(true);
+            }
+
+            setTimeout(() => {
+                setShowPostMessage(false);
+            }, 2000);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <div className={style.calendarWrap}>
             <div className={style.title}>Current Date:</div>
 
-
-            <input
-                value={calendar}
-                readOnly
-                className={style.inputBox}
-                onClick={() => setOpen((open) => !open)}
-            />
-
-            <div ref={refOne}>
-                {open && (
-                    <Calendar
-                        date={new Date()}
-                        onChange={() => setOpen(false)}
-                        className={style.calendarElement}
+            <div className={style.currentDateForm}>
+                <div className={style.calendarInput}>
+                    <input
+                        value={calendar}
+                        readOnly
+                        className={style.inputBox}
+                        name="date"
+                        onClick={() => setOpen((open) => !open)}
                     />
-                )}
+
+                    <div ref={refOne}>
+                        {open && (
+                            <Calendar
+                                date={new Date()}
+                                onChange={() => setOpen(false)}
+                                className={style.calendarElement}
+                            />
+                        )}
+                    </div>
+                </div>
+                <button
+                    type="button"
+                    className={style.sendDateBtn}
+                    onClick={sendDateHandler}
+                >
+                    SEND
+                </button>
             </div>
+
+            {showPostMessage && (
+                <div className={style.postMessage}>{postMessage}</div>
+            )}
         </div>
     );
 };
